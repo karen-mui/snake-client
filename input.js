def219f5629155b5190ea6919d5fc849dcd53fac
Karen Mui <karen_mui@hotmail.com>
2022-11-25 19:47:12 +0000

    program WASD/up, left, down, right movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,10 +11,27 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+
 };
 
-module.exports = setupInput
+module.exports = setupInput;
