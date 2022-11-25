849663d45d81157c9b30dad042205de4ca7271ff
Karen Mui <karen_mui@hotmail.com>
2022-11-25 00:20:57 +0000

    connection to the server

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+conn.on("connect", () => {
+  console.log('You have succesfully connected!')
+});
